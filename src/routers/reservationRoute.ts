52a63a1acac9e6301fdde1ec544a747008040e35
Marcell <marcelltemleitner@gmail.com>
2026-08-01 15:55:48 +0200

    CancelReservation route with view

--- src/routers/reservationRoute.ts
+++ src/routers/reservationRoute.ts
@@ -6,6 +6,7 @@ const router = Router();
 router.get("/api/v1/getAllReservation", ReservationController.getAllReservation);
 router.post("/api/v1/CreateReservation", ReservationController.createReservation);
 router.get("/api/v1/myReservation/:userId", ReservationController.getUserReservations);
+router.patch("/api/v1/CancelReservation/:reservationId", ReservationController.CancelReservation);
 
 
 export default router;
